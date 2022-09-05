1c0a4acd93278de5017d2fac2705740bd06b91a2
Paweł Łęczycki <pawel@msd.info.pl>
2022-09-05 10:21:07 +0200

    StaticFiles

--- WebAppMVC/Program.cs
+++ WebAppMVC/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +15,22 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    //using Microsoft.Extensions.FileProviders;
+    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "Downloads")),
+    RequestPath = "/pliki",
+
+    OnPrepareResponse = x => x.Context.Response.Headers.Append("Cache-Control", "public, max-age=60000")
+});
+
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "Downloads")),
+    RequestPath = "/pliki"
+
+});
 
 app.UseRouting();
 
