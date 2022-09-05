f5fc8fdc26852eff1fc54289eeaa1cbedd09cedd
Paweł Łęczycki <pawel@msd.info.pl>
2022-09-05 13:59:55 +0200

    UserController _entities

--- WebAppMVC/Controllers/UsersController.cs
+++ WebAppMVC/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ namespace WebAppMVC.Controllers
     public class UsersController : Controller
     {
         private ICrudService<User> _service;
+        //private IEnumerable<User> _entities = new List<User> { new User() { Id = 1, Username = "a", Password = "b" } };
 
         public UsersController(ICrudService<User> service)
         {
@@ -15,6 +16,7 @@ namespace WebAppMVC.Controllers
 
         public async Task<IActionResult> Index()
         {
+            //return View(_entities);
             return View(await _service.ReadAsync());
         }
     }
